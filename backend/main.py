615fb2179fac677e6b823accf474fb24c175765b
james harvey <jamesharvey@james-harveys-MacBook-Pro-2.local>
2025-07-22 22:06:53 -0400

    Updated frontend to use live backend URL

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 # 👇 Add this to allow frontend to talk to backend
 origins = [
-    "https://https://avatar-chat-gp89ffmet-james-projects-65c1adad.vercel.app",  # your deployed Vercel URL
+    "https://https://avatar-chat-app-puce.vercel.app",  # your deployed Vercel URL
 ]
 
 app.add_middleware(
